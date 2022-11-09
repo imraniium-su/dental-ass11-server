a6aa93c37de748095c5b6e0625a612c7ac215c6d
MD IMRAN <imran2021ia@gmail.com>
2022-11-09 10:27:32 +0800

    add async function

--- index.js
+++ index.js
@@ -15,12 +15,11 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mo0btsm.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
 
+async function run() {
+
+}
+run().catch(err => console.error(err));
 
 
 app.get('/', (req, res) => {
